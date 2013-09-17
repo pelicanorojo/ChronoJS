ded60e1277c203804c549872a75823a2f4ad9098
U-Admin-PC\Admin <Admin@Admin-PC.(none)>
2013-09-17 09:38:10 -0300

    Documentation redo, big refactor.

---
+++ ChronoJs-TimeBeats.js
@@ -0,0 +1,230 @@
+/**
+ * LIBRARY: ChronoJS
+ *
+ * https://github.com/pelicanorojo/ChronoJS
+ *
+*/
+/**
+ * MODULE: TimeBeats
+ *
+ * You can use CRNJS.timeBeats as a singleton object, or as a prototype for a Constructor Function, Factory Function, backbone model or other framework.
+ * Or you can use the constructor function CRNJS.TimeBeats.
+ * Are pending commonJS and AMD versions.
+ *
+ */
+(function ( global ) {
+	var
+		CRNJS = global.CRNJS = global.CRNJS || {};
+
+	function dummyFn () {}
+
+	CRNJS.timeBeats = {
+		statesValues: {
+			stopped: 'stopped',
+			started: 'started',
+			paused: 'paused'
+		},
+		initialize: function ( options ) {
+			options = options || {};
+
+			this.setPeriod( options.period || this.period || 0 );//beat period
+			this.setCallback( options.callback || dummyFn );
+
+			//desired ticks count
+			//one tick by default, tick must be undefined or Natural number. 0 for infinite ticks
+			this.setTicks( ( typeof options.ticks !== 'undefined' ) ? options.ticks : this.ticks || 1 );
+
+			this._reset();
+
+			return this;
+		},
+		_deleteTimer: function () {
+			if ( this.interval !== null ) {
+				if ( this.inPadding ) {
+					window.clearTimeout( this.interval );
+					this.inPadding = false;
+				} else {
+					window.clearInterval( this.interval );
+				}
+				this.interval = null;
+			}
+		},
+		_periodicFunction: function ( callback ) {
+			var
+				lastBeat = false;
+
+				this.lastTick = new window.Date();
+
+				if ( this.remainingTicks !== Number.POSITIVE_INFINITY ) { //if not infinite repetitions
+					lastBeat = this.remainingTicks === 0;
+					if ( !lastBeat ) {
+						this.remainingTicks--;
+					}
+				}
+
+				if ( lastBeat ) {
+					this._reset();
+				}
+				// call after eventual reset for good remainder time calculation
+				try {
+					//protect against callback error
+					callback( lastBeat );
+				}  catch ( e ) {
+					//TODO: securelog
+				}
+		},
+		_createPaddingTimeout: function ( callback ) {
+			var
+				that = this;
+
+			this.lastTick = new window.Date() - ( this.pauseTick - this.lastTick ); //compensating
+
+			this.pauseTick = null;
+
+			this.inPadding = true;
+			this.interval = window.setTimeout( function () {
+				that._periodicFunction( callback ); // the intermediary callback
+			}, this.getRemainingTickTime() );//TODO: check how are treated negatives values, because it may come on resumes with shorter periods.	
+		},
+		_createTimer: function () {
+			var
+				that = this;
+
+			this.interval = window.setInterval( function () {
+				that._periodicFunction( that.callback ); // the user callback
+			}, this.period );
+		},
+		_reset: function () {
+			this.state = this.statesValues.stopped;
+
+			this._deleteTimer();				
+
+			this.lastTick = null; // the last tick in order to calculate the remaining tick time
+			this.pauseTick = null; // the pause in order to calculate the remaining tick time
+
+			return this;
+		},
+		start: function ( options ) {
+			this.state = this.statesValues.started;
+
+			this.remainingTicks = this.ticks ? this.ticks-1 : Number.POSITIVE_INFINITY;
+
+			this.lastTick = new window.Date();
+			if ( options && options.callBackOnStart ) { this.callback( false/*lastBeat*/, true/*startBeat*/ ); }
+			this._createTimer();
+
+			return this;
+		},
+		stop: function () {
+			this._reset();
+			return this;
+		},
+		pause: function () {
+			this.state = this.statesValues.paused;
+
+			this._deleteTimer();
+			this.pauseTick =  new window.Date();
+			return this;
+		},
+		resume: function ( options ) {//TODO: add ticks on options.
+			var 
+				that = this;
+
+			options = options || {};
+
+			if ( this.state === this.statesValues.paused ) {
+				this.state = this.statesValues.started;			
+				this.callback = options.callback || this.callback;
+
+				//change period before resume
+				if ( !options.setAfterResume ) {
+					this.period = options.period || this.period;
+				}
+
+				//ends current tick
+				this._createPaddingTimeout( function ( lastBeat ) {
+
+					//change period after resume
+					if ( !options.setAfterResume ) {
+						that.period = options.period || that.period;
+					}
+
+					//restart timer
+					if ( !lastBeat ) {
+						that._createTimer();
+					}
+					//call callback
+					that.callback( lastBeat );
+				});
+			}
+			return this;
+		},
+		getState: function () {
+			return this.state;
+		},
+		setPeriod: function ( period ) {
+			this.period = period;
+			return this;
+		},
+		getPeriod: function () {
+			return this.period;
+		},
+		setTicks: function ( ticks ) {
+			ticks = window.Math.round( window.Math.abs( ticks ) );
+			this.ticks = ticks;
+			return this;
+		},
+		getTicks: function () {
+			return this.ticks;
+		},
+		setCallback: function ( callback ) {
+			this.callback = callback;
+			return this;
+		},
+		getRemainingTicks: function () {
+			return ( this.state !== this.statesValues.stopped ) ? this.remainingTicks : 0 ;
+		},
+		addRemainingTicks: function ( ticks ) {
+			if ( this.state !== this.statesValues.stopped ) {
+				this.remainingTicks += ticks;
+			}
+			return this;
+		},	
+		getRemainingTickTime: function () {
+			var remaining;
+
+			if ( this.state !== this.statesValues.stopped ) {
+				remaining = this.pauseTick ? 
+				this.period - ( this.pauseTick - this.lastTick ) :
+				this.period - ( new window.Date() - this.lastTick );
+			} else {
+				remaining = 0;
+			}
+
+			return remaining;		
+		},
+		getRemainingTime: function () {
+			var remaining;
+
+			if ( this.remainingTicks === Number.POSITIVE_INFINITY ) {
+				remaining = -1;			
+			} else if ( this.state === this.statesValues.stopped ) {
+				remaining = 0;
+			} else {
+				remaining = this.getRemainingTickTime() + ( this.remainingTicks ) * this.period;
+			}
+
+			return remaining;
+		}
+	};
+
+	CRNJS.TimeBeats = function ( period, callback, options ) {
+		//If call without arguments initialize the instance.
+		if ( arguments.length ) {
+			this.initialize.apply( this, arguments );
+		}
+		return this;
+	}
+
+	CRNJS.TimeBeats.prototype = CRNJS.timeBeats;
+}( this ));
--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 ##ChronoJS
 ========
 
-Javascript - Backbone - Timer
+Javascript - Time Control Library
 
-This is a Backbone model for manage timers with a periodic ticks.
+This is a JS Librarly for manage time. Oh yes Albert, the time!
+
+It have 2 modules by now, TimeBeats and babySteps.
+
+## CRNJS.TimeBeats Module
 
 The main features are its ability for pause the timer in middle of a period,
 and resume the paused period and the subsecuent ticks.
@@ -11,42 +15,114 @@ and resume the paused period and the subsecuent ticks.
 
 ###Methods
 
+#####initialize
+
 ```javascript
-initialize: function (period, callback, options)
+initialize: function ( options )
 returns: this
 ```
-
-__period__ is the tick's period
-
-__callback__ is for call at the end of each period. Now a boolean param lastBeat is passed in the last one!!
 
 __options__ is an optional object with this properties:
 
 * ```ticks``` _number_: how much periods do you want. (default is 1, and if you pass 0 it means infinity)
-* ```start``` _function_: starts the timer.
-* ```stop``` _function_: stops the timer.
-* ```pause``` _function_: pause the timer
-* ```resume``` _function_: resume the timer
-_the resume methods firt do de partially done period, and then continue with the whole remaining periods._
-// deprecated: "and allow to pass optional new period and callback params"
-* ```getRemainingTicks``` _function_: returns the remaining whole ticks.
-* ```getRemainingTickTime``` _function_: returns the current period remaining time.
-* ```getRemainingTime``` _function_: returns the timer remaining time, that is the current period remaining more the remaining ticks times.
-* ```getState``` _function_: returns the state string: stopped, started or paused. Is a result of a better chrono state manage.
-* ```getTicks``` _function_ returns the ticks ( not the remaining else the used for start the timer)
-* ```setTicks``` _function_ (ticks) return: this ( not the remaining else the used for start the timer )
-* ```getPeriod``` _function_ returns the period
-* ```setPeriod``` _function_ //for use in stopped state, I'll fix this soon.... returns this
-* ```addRemainingTicks``` _function_ If not in stopped state, adds remaining ticks. return this	
+
+* ```period``` _number_: is the tick's period in milliseconds.
+
+* ```callback``` _function_: Is called at the end of each period. And, if started with callBackOnStart, is called at the first period start with ( false/*lastBeat*/, true/*startBeat*/ ) arguments. With finites ticks, at the end of the last one, is called with ( true/*lastBeat*/, undefined/*startBeat*/ ) arguments.
+
+##### start
+
+```javascript
+start: function ( options )
+returns: this
+```
+
+__options__ is an optional object with this properties:
+
+* ```callBackOnStart``` _boolean_: if true, the callback is called on start, at time 0.
+
+##### stop
+
+```javascript
+stop: function ()
+returns: this
+```
+
+##### pause
+
+```javascript
+pause: function ()
+returns: this
+```
+
+##### resume
+
+Resumes the timeBeats after paused.
+
+```javascript
+resume: function (  options  )
+returns: this
+```
+
+__options__ is an optional object with this properties:
+
+* ```period``` _number_: the new optional period in milliseconds.
+
+* ```callback``` _function_: an optional new callback overrider callback.
+
+* ```setAfterResume``` _boolean_: if true, the new period is apply after the finalization of the current truncated period, if alse is set prior to the finalization and is used for it.
+( May be there is a apriori bug when you short the period, with setAfterResume in true, except if setTimeout interpret negatives numbers as zero. Or you are using CronoJs in your time machine ;). ) TODO: modified ticks number too.
+
+##### getRemainingTicks
+
+returns the remaining whole ticks.
+
+##### getRemainingTickTime
+
+returns the current period remaining time.
+
+##### getRemainingTime
+
+returns the timer remaining time, that is the current period remaining more the remaining ticks times.
+
+##### getState
+
+returns the state string: stopped, started or paused. Is a result of a better chrono state manage.
+
+##### getTicks
+
+returns the ticks ( not the remaining else the used for start the timer )
+
+##### setTicks
+
+returns this ( set the ticks, not the remaining else the used for start the timer ).
+
+##### getPeriod
+
+returns the period.
+
+##### setPeriod
+
+//for use in stopped state, I'll fix this soon.... returns this
+
+##### addRemainingTicks
+
+If not in stopped state, adds remaining ticks. return this	
 
 ### Example of use
 
 ```javascript
-var timer = initialize(5, done, { ticks: 0});
-function done(){
-  //I'm initialized!
-  timer.start();
+var timeBeat = new CRNJS.TimeBeats({
+	ticks: 5,
+	period: 5000,
+	callback: tickFn
+});
+
+function tickFn ( lastBeat, startCall ) {
+  console.log( 'tickFn ( startCall, lastBeat ) = ( ' + startCall + ', ' + lastBeat + ' )');
 }
+
+timeBeat.start( { callBackOnStart: true } );
 ```
 _**Checkout the index.html file to see it working on a real Backbone example**_
 
